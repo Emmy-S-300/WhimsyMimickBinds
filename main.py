5cfb142d316e1786dbac86896d59b15c6242c6e6
Emmy Schonau <emmy.schonau@gmail.com>
2026-06-27 20:32:22 -0400

    Added the parsing function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# from this library, import the beautifulsoup class
+from bs4 import BeautifulSoup
+
+
 #function that takes file_path as input
 def read_html_file(file_path):
     
@@ -10,8 +14,18 @@ def read_html_file(file_path):
     #returns the html string
     return html_content
 
-#test the function by storing the returned hmtl in the variable html
-html = read_html_file("sample.html")
+#Convert the html string into something python can search through.
+def parse_story_html(html_content):
+    #Use BeautifulSoup to parse the html and create a searchable object
+    soup = BeautifulSoup(html_content, "html.parser")
 
-#printing the first 500 chars of the content
-print(html[:500])
+    #find the 1st h1 elemnt and extract only its text
+    title = soup.find("h1").get_text(strip=True)
+
+    #print the variable
+    print(title)
+
+# read the sample html
+html = read_html_file("sample.html")
+#parse the html
+parse_story_html(html)
